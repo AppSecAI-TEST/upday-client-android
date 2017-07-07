f3c14e9751a8d28889a4bfa8da329ffc6ab83514
Damien Chomat <damien@smartnsoft.com>
2017-07-07 10:15:23 +0200

    ADDED - Crash reporting - Crashlytics integration

--- app/src/main/java/com/axelspringer/upday/UpdayApplication.java
+++ app/src/main/java/com/axelspringer/upday/UpdayApplication.java
@@ -5,6 +5,9 @@ import android.app.Application;
 import com.axelspringer.upday.infrastructure.di.components.DaggerNetworkComponent;
 import com.axelspringer.upday.infrastructure.di.components.NetworkComponent;
 import com.axelspringer.upday.infrastructure.di.modules.NetworkModule;
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by damien on 7/4/17.
@@ -19,6 +22,7 @@ public class UpdayApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         mApplication = this;
         networkComponent = DaggerNetworkComponent.builder()
                 .networkModule(new NetworkModule(BuildConfig.BASE_URL))
